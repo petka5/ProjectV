447e1d162caf0ded5c93fbc6c9f9dbca385c57cd
Petka <petar.georgiev@gmail.com>
2019-07-17 22:29:00 +0300

    Added lombok, application.properties banner and logback config.

--- backend/src/main/java/org/petka/backend/controllers/HelloController.java
+++ backend/src/main/java/org/petka/backend/controllers/HelloController.java
@@ -3,11 +3,15 @@ package org.petka.backend.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
+        log.info("Hello world controller.");
         return "Greetings from Spring Boot!";
     }
 }
